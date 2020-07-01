3a4b83d629e4aadaf6417c5be0df97d1eda7be8c
youkan <youkan94@gmail.com>
2020-07-01 15:34:01 +0900

    feat: ページを追加

---
+++ src/containers/pages/Top.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from '../organisms/Head';
+import Header from '../organisms/Header';
+
+function Top() {
+  return (
+    <div className="Top">
+        <Head />
+        <Header />
+        <h1>Top</h1>
+    </div>
+  );
+}
+
+export default Top;
