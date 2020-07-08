ea1090749d31ac294b98226532679e8ac25503f1
youkan <youkan94@gmail.com>
2020-07-08 16:10:02 +0900

    feat: マークダウン用のスタイルを追加

--- src/styles/foundation/StyleFoundation.tsx
+++ src/styles/foundation/StyleFoundation.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Reset from './Reset';
 import Foundation from './Foundation';
+import Markdown from './Markdown';
 
 function StyleFoundation() {
   return (
     <>
       <Reset />
       <Foundation />
+      <Markdown />
     </>
   );
 }
