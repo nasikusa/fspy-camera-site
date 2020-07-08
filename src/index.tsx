ef3deb7c81aa756c4b3f88f9e511bbc55f62c856
youkan <youkan94@gmail.com>
2020-07-08 16:13:25 +0900

    fix: IE11対応

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
